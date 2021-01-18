fb4628f40d5c099bd5c3c0e55d78a1d06d029667
kobiyoyo <adamachubi@gmail.com>
2021-01-18 19:25:46 +0100

    fix bunler error

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(first_name: 'dan',last_name: 'adama',password:'01234Admin',email:'admin@admin.com')
- 
+  
